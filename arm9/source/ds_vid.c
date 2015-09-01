bd85a9de7a62ca0fd3c884b185c4badaafa59b9f
elhobbs <elhobbs@comcast.net>
2015-09-01 07:33:11 -0400

    fixed keymapping for zl/zr buttons

--- arm9/source/ds_vid.c
+++ arm9/source/ds_vid.c
@@ -42,22 +42,22 @@ touchPosition	g_currentTouch = { 0, 0 };
 
 //0=DS Bit,1=game key, 2=menu key
 int keys3ds[32][3] = {
-	{ KEY_A, KEYD_RCTRL, KEYD_ENTER }, //bit 00
-	{ KEY_B, ' ', KEYD_ESCAPE }, //bit 01
-	{ KEY_SELECT, KEYD_ENTER, 0 }, //bit 02
-	{ KEY_START, KEYD_ESCAPE, 0 }, //bit 03
-	{ KEY_DRIGHT, KEYD_RIGHTARROW, 0 }, //bit 04
-	{ KEY_DLEFT, KEYD_LEFTARROW, 0 }, //bit 05
-	{ KEY_DUP, KEYD_UPARROW, 0 }, //bit 06
-	{ KEY_DDOWN, KEYD_DOWNARROW, 0 }, //bit 07
-	{ KEY_R, '.', 0 }, //bit 08
-	{ KEY_L, ',', 0 }, //bit 09
-	{ KEY_X, KEYD_RSHIFT, 0 }, //bit 10
-	{ KEY_Y, KEYD_RCTRL, 'y' }, //bit 11
+	{ KEY_A,		KEYD_RCTRL,		KEYD_ENTER }, //bit 00
+	{ KEY_B,		' ',			KEYD_ESCAPE }, //bit 01
+	{ KEY_SELECT,	KEYD_ENTER,		0 }, //bit 02
+	{ KEY_START,	KEYD_ESCAPE,	0 }, //bit 03
+	{ KEY_DRIGHT,	KEYD_RIGHTARROW,0 }, //bit 04
+	{ KEY_DLEFT,	KEYD_LEFTARROW, 0 }, //bit 05
+	{ KEY_DUP,		KEYD_UPARROW,	0 }, //bit 06
+	{ KEY_DDOWN,	KEYD_DOWNARROW, 0 }, //bit 07
+	{ KEY_R,		'.',			0 }, //bit 08
+	{ KEY_L,		',',			0 }, //bit 09
+	{ KEY_X,		'x',			0 }, //bit 10
+	{ KEY_Y,		'y',			'y' }, //bit 11
 	{ 0, 0, 0 }, //bit 12
 	{ 0, 0, 0 }, //bit 13
-	{ KEY_ZL, KEYD_RCTRL, 0 }, //bit 14
-	{ KEY_ZR, KEYD_RCTRL, 0 }, //bit 15
+	{ KEY_ZL,		'<',			0 }, //bit 14
+	{ KEY_ZR,		'>',			0 }, //bit 15
 	{ 0, 0, 0 }, //bit 16
 	{ 0, 0, 0 }, //bit 17
 	{ 0, 0, 0 }, //bit 18
@@ -66,14 +66,14 @@ int keys3ds[32][3] = {
 	{ 0, 0, 0 }, //bit 21
 	{ 0, 0, 0 }, //bit 22
 	{ 0, 0, 0 }, //bit 23
-	{ KEY_CSTICK_RIGHT, KEYD_CSTICK_RIGHT, 0 }, //bit 24
-	{ KEY_CSTICK_LEFT, KEYD_CSTICK_LEFT, 0 }, //bit 25
-	{ KEY_CSTICK_UP, KEYD_CSTICK_UP, 0 }, //bit 26
-	{ KEY_CSTICK_DOWN, KEYD_CSTICK_DOWN, 0 }, //bit 27
-	{ KEY_CPAD_RIGHT, KEYD_CPAD_RIGHT, 0 }, //bit 28
-	{ KEY_CPAD_LEFT, KEYD_CPAD_LEFT, 0 }, //bit 29
-	{ KEY_CPAD_UP, KEYD_CPAD_UP, 0 }, //bit 30
-	{ KEY_CPAD_DOWN, KEYD_CPAD_DOWN, 0 }, //bit 31
+	{ KEY_CSTICK_RIGHT,	KEYD_CSTICK_RIGHT,	0 }, //bit 24
+	{ KEY_CSTICK_LEFT,	KEYD_CSTICK_LEFT,	0 }, //bit 25
+	{ KEY_CSTICK_UP,	KEYD_CSTICK_UP,		0 }, //bit 26
+	{ KEY_CSTICK_DOWN,	KEYD_CSTICK_DOWN,	0 }, //bit 27
+	{ KEY_CPAD_RIGHT,	KEYD_CPAD_RIGHT,	0 }, //bit 28
+	{ KEY_CPAD_LEFT,	KEYD_CPAD_LEFT,		0 }, //bit 29
+	{ KEY_CPAD_UP,		KEYD_CPAD_UP,		0 }, //bit 30
+	{ KEY_CPAD_DOWN,	KEYD_CPAD_DOWN,		0 }, //bit 31
 };
 
 void DS_Controls(void) {
